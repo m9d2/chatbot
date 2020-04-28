09a9cf3358b8e91ff1a982874e52ee938a480960
gaoyang <gaoyangyang@innjoy.me>
2020-04-28 10:02:11 +0800

    update

--- src/main/java/com/gy/chatbot/controller/WechatBotController.java
+++ src/main/java/com/gy/chatbot/controller/WechatBotController.java
@@ -1,16 +1,20 @@
 package com.gy.chatbot.controller;
 
-import com.gy.chatbot.bean.Wechat;
 import com.gy.chatbot.bean.WechatContact;
 import com.gy.chatbot.common.context.UserContext;
+import com.gy.chatbot.common.utils.JsonUtils;
 import com.gy.chatbot.service.WechatBotService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+
 @Controller
+@Slf4j
 @RequestMapping("/")
 public class WechatBotController {
 
@@ -40,30 +44,37 @@ public class WechatBotController {
                 wechatBotService.start();
                 WechatContact wechatContact = wechatBotService.getContact();
                 model.addAttribute(wechatContact);
-                return "contact";
+                return "redirect: /contact";
             }
             if(ret == 408) {
-                return "error";
+                return "redirect: /error";
             } else {
-                return "error";
+                return "redirect: /error";
             }
         }
-        return "error";
+        return "redirect: /error";
     }
 
     @RequestMapping("contact")
-    public String getContact(Model model) {
-    	Wechat wechat = UserContext.getWechat();
-    	if(wechat == null) {
-            return "error";
-    	}
-        WechatContact wechatContact = wechatBotService.getContact();
+    public String getContact(Model model) throws IOException {
+//    	Wechat wechat = UserContext.getWechat();
+//    	if(wechat == null) {
+//            return "error";
+//    	}
+//        WechatContact wechatContact = wechatBotService.getContact();
+        WechatContact wechatContact = JsonUtils.readJsonFromClassPath("/data.json", WechatContact.class);
         if (null != wechatContact) {
             model.addAttribute("contact", wechatContact);
             return "contact";
         } else {
             return "error";
         }
+    }
+
+    @GetMapping("send")
+    public Integer sendMessage(String msg) {
+        log.info(msg);
+        return 200;
     }
 
     @RequestMapping("logout")
